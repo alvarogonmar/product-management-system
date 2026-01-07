2b026f65e0b71f4448889593ba0fd0ae5a0a1780
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-06 21:52:37 -0600

    feat: add CRUD routes for basic API functionality in server

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,26 @@ import express from "express"
 
 const server = express();
 
+// Routing
+server.get("/", (req, res) => {
+    res.json("Hello, World!");
+})
+
+server.post("/", (req, res) => {
+    res.json({"Hello, World!": "POST"});
+})
+
+server.put("/", (req, res) => {
+    res.json({"Hello, World!": "PUT"});
+})
+
+server.patch("/", (req, res) => {
+    res.json({"Hello, World!": "PATCH"});
+})
+
+server.delete("/", (req, res) => {
+    res.json({"Hello, World!": "DELETE"});
+});
+
+
 export default server;
