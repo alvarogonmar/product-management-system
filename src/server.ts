1c5aa81483f3baa3266d983554945ed4e68b849e
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-07 18:28:26 -0600

    feat: refactor server setup and remove unused routes

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,20 @@
 import express from "express"
+import router from "./router";
+import db from "./config/db";
 
+// Test DB connection
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log('Database connected successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:');
+    }
+}
+
+connectDB()
 const server = express();
-
-// Routing
-server.get("/", (req, res) => {
-    res.json("Hello, World!");
-})
-
-server.post("/", (req, res) => {
-    res.json({"Hello, World!": "POST"});
-})
-
-server.put("/", (req, res) => {
-    res.json({"Hello, World!": "PUT"});
-})
-
-server.patch("/", (req, res) => {
-    res.json({"Hello, World!": "PATCH"});
-})
-
-server.delete("/", (req, res) => {
-    res.json({"Hello, World!": "DELETE"});
-});
-
+server.use('/api/productos', router);
 
 export default server;
