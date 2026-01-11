4b126c47000a06d92056d55411320faa67a4afd4
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-10 20:00:49 -0600

    fix: remove redundant comment for 'name' field validation in POST request

--- src/router.ts
+++ src/router.ts
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
 
 router.post("/", 
         // Validate request body 
-    body('name').notEmpty().withMessage('Name product is required'), // Validate 'name' field
+    body('name').notEmpty().withMessage('Name product is required'),
 
     body('price') // Validate 'price' field
         .isNumeric().withMessage('Price product must be a number') 
