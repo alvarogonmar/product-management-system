9c50597ee0dd56d5cb19043cc45637f24a7c28ca
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-12 21:48:54 -0600

    fix: implement updateProduct handler to retrieve and return product by ID

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -36,5 +36,11 @@ export const createProduct = async (req: Request, res: Response) => {
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
-    res.json({"Hello, World!": "PUT"});
+    const { id } = req.params;
+    const product =  await Product.findByPk(id);
+    if(!product){
+        return res.status(404).json({error: 'Product not found'});
+    }
+    res.json({data: product});
+
 }
