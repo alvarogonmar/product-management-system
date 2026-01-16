c053103c2949ae691c98cc120ceddbbb6ad5ebfc
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-15 22:02:04 -0600

    test: add request body to POST /api/productos test

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -3,6 +3,7 @@ import server from "../../server";
 
 describe("POST /api/productos", () => {
     it("should create a new product", async () => {
+        const response = await request(server).post("/api/productos").send({
 
         });
     });
