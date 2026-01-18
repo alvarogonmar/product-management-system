9407a0da0e96a87d81cda33eb226dc57c4c8acb6
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-18 17:08:34 -0600

    refactor: remove unnecessary comment in PUT request test for updating product price validation

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -115,7 +115,7 @@ describe("PUT /api/productos/:id", () => {
 
 
     it("should validate that the price is greater than 0 when updating a product", async () => {
-        const response = await request(server) // response variable
+        const response = await request(server)
             .put("/api/productos/1")
             .send({
                 name: "Updated Product",
