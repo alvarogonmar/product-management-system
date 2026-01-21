2877334c6122febe6f878baf1b2e18a61411f8ad
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-20 21:38:20 -0600

    feat: update swagger options to include openapi version and info structure

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,3 +1,8 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
-const options: swaggerJSDoc.Options = {};
+const options: swaggerJSDoc.Options = {
+    swaggerDefinition: {
+        openapi: "3.0.0",
+        info: {
+            
+};
