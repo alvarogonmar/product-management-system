942dc6f7afdaec4476ceb3b43ddedf3021b481ae
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-20 21:42:25 -0600

    refactor: remove unused API endpoint test and related code

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,19 +1,5 @@
-import server, { connectDB } from "../server";
-import request from "supertest";
+import { connectDB } from "../server";
 import db from "../config/db";
-
-describe("GET /api", () => {
-    it("shoould send back a json with a message", async () => {
-        const res = await request(server).get("/api");
-
-        expect(res.status).toBe(200);
-        expect(res.headers['content-type']).toMatch(/json/);
-        expect(res.body.msg).toBe("From API")
-
-        expect(res.status).not.toBe(404);
-        expect(res.body.msg).not.toBe("from api");
-    });
-});
 
 jest.mock("../config/db");
 
