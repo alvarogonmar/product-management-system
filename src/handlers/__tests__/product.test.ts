b17ecee6848181602f859ab3565734667b00a972
alvarogonmar <alvarogonmar0@gmail.com>
2026-01-28 21:01:00 -0600

    Deleting comments

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -184,7 +184,7 @@ describe("PUT /api/productos/:id", () => {
 });
 
 describe("PATCH /api/productos/:id", () => {
-    it("should return a 404 response for a non existent product", async () => { // Missing ID validation test
+    it("should return a 404 response for a non existent product", async () => {
         const productID = 2000 // Non-existent product ID
         const response = await request(server) // Make the PATCH request
             .patch(`/api/productos/${productID}`); //PATCH endpoint to toggle availability
