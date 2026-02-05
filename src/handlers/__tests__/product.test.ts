ee892d2a64ac5ac946749de32ea9c1e9915bfb6b
alvarogonmar <alvarogonmar0@gmail.com>
2026-02-04 21:37:46 -0600

    fix: remove unnecessary comment in PATCH request test for product availability

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -201,7 +201,7 @@ describe("PATCH /api/productos/:id", () => {
             .patch(`/api/productos/1`);
 
         expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("data"); // Check for data property
+        expect(response.body).toHaveProperty("data");
         expect(response.body.data.availability).toBe(false); // Check if availability is updated
 
         expect(response.status).not.toBe(404);
